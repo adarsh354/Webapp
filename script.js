0bf97cf3f19a543d5a7665a3a41a0debe105ea88
adarsh354 <37932533+adarsh354@users.noreply.github.com>
2018-04-28 20:17:02 +0530

    done
    done

---
+++ script.js
@@ -0,0 +1,9 @@
+function myfunction()
+{
+var a=parseInt(document.getElementById("a").value);
+var b=parseInt(document.getElementById("b").value);
+var c=a+b;
+
+document.getElementById("sum").value=c;
+}
+
